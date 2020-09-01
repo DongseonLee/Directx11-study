9c937fb1a1ecb916ab06a9c6fff8685e8101d676
DongseonLee <lds3573@naver.com>
2020-09-02 00:16:40 +0900

    [chapter6] MathHelper class 추가
    MathHelper class 추가, 6장부터 사용됨.

---
+++ Directx11/Directx11/MathHelper.cpp
@@ -0,0 +1,10 @@
+//***************************************************************************************
+// MathHelper.cpp by Frank Luna (C) 2011 All Rights Reserved.
+//***************************************************************************************
+
+#include "MathHelper.h"
+#include <float.h>
+#include <cmath>
+
+const float MathHelper::Infinity = FLT_MAX;
+const float MathHelper::Pi = 3.1415926535f;
